a5b72bc78ff8f67854c2e824229478a44a0bbdf0
WooKyoung Noh <wookay.noh@gmail.com>
2019-11-30 01:22:35 +0900

    datadragon add ko_KR

--- gen/generator.jl
+++ gen/generator.jl
@@ -1,22 +1,43 @@
+# https://developer.riotgames.com/docs/lol#data-dragon
+#=
+wget http://ddragon.leagueoflegends.com/cdn/9.23.1/data/en_US/champion.json -P en_US
+wget http://ddragon.leagueoflegends.com/cdn/9.23.1/data/ko_KR/champion.json -P ko_KR
+=#
+
+locales = ("en_US", "ko_KR")
+
 using JSON2
 
-# https://developer.riotgames.com/docs/lol#data-dragon
-# wget http://ddragon.leagueoflegends.com/cdn/9.23.1/data/en_US/champion.json
-
-nt = JSON2.read(read(normpath(@__DIR__, "champion.json"), String))
-
+nt = JSON2.read(read(normpath(@__DIR__, first(locales), "champion.json"), String))
 champions_jl = normpath(@__DIR__, "Champions.jl")
 f = open(champions_jl, "w")
 write(f, "# generated\n")
 write(f, "type = ", repr(nt.type), "\n")
 write(f, "format = ", repr(nt.format), "\n")
 write(f, "version = ", repr(VersionNumber(nt.version)), "\n")
-
-write(f, "Champions = Dict{Int,NamedTuple}(\n")
-for c in nt.data
-    championId = parse(Int, c.key)
-    write(f, repeat(' ', 4), repr(championId => c), ",\n")
+write(f, """
+for locale in $(repr(locales))
+    include(normpath(@__DIR__, locale, "data.jl"))
 end
-write(f, ")\n")
-
+""")
 close(f)
+
+for locale in locales
+    data_jl = normpath(@__DIR__, locale, "data.jl")
+    if locale == first(locales)
+        global nt
+    else
+        nt = JSON2.read(read(normpath(@__DIR__, locale, "champion.json"), String))
+    end
+    f = open(data_jl, "w")
+    write(f, "# generated\n")
+    write(f, "module $locale\n")
+    write(f, "Champions = Dict{Int,NamedTuple}(\n")
+    for c in nt.data
+        championId = parse(Int, c.key)
+        write(f, repeat(' ', 4), repr(championId => c), ",\n")
+    end
+    write(f, ")\n")
+    write(f, "end # module $locale\n")
+    close(f)
+end
--- test/loltools/datadragon.jl
+++ test/loltools/datadragon.jl
@@ -3,15 +3,24 @@ module test_loltools_datadragon
 using Test
 using LOLTools.DataDragon
 
-@test DataDragon.version == v"9.22.1"
+@test DataDragon.version == v"9.23.1"
 
-champ = DataDragon.Champions[126]
+champ = DataDragon.en_US.Champions[126]
 @test champ.name == "Jayce"
 
-champ = DataDragon.Champions[13]
+champ = DataDragon.en_US.Champions[13]
 @test champ.name == "Ryze"
 
-champ = DataDragon.Champions[114]
+champ = DataDragon.en_US.Champions[114]
 @test champ.name == "Fiora"
+
+champ = DataDragon.ko_KR.Champions[126]
+@test champ.name == "제이스"
+
+champ = DataDragon.ko_KR.Champions[13]
+@test champ.name == "라이즈"
+
+champ = DataDragon.ko_KR.Champions[114]
+@test champ.name == "피오라"
 
 end # module test_loltools_datadragon
