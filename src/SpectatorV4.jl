ac726cce9a21c68ab5986459eed61f71105d08da
WooKyoung Noh <wookay.noh@gmail.com>
2019-11-30 18:51:20 +0900

    active_games

--- src/SpectatorV4.jl
+++ src/SpectatorV4.jl
@@ -9,11 +9,34 @@ struct FeaturedGames <: AbstractDTO
     FeaturedGames(clientRefreshInterval, gameList) = new(clientRefreshInterval, gameList)
 end
 
+struct CurrentGameInfo <: AbstractDTO
+    gameId
+    gameStartTime
+    platformId
+    gameMode
+    mapId
+    gameType
+    bannedChampions
+    observers
+    participants
+    gameLength
+    gameQueueConfigId
+    CurrentGameInfo(gameId, gameStartTime, platformId, gameMode, mapId, gameType, bannedChampions, observers, participants, gameLength, gameQueueConfigId) = new(gameId, gameStartTime, platformId, gameMode, mapId, gameType, bannedChampions, observers, participants, gameLength, gameQueueConfigId)
+end
+
 function featured_games(api_key::String,
                         region::String ;
                         endpoint::HTTP.URI = lol_api_server(region),
                         action::Function = http_action)::FeaturedGames
     call_api(FeaturedGames, api_key, action, endpoint, "/lol/spectator/v4/featured-games")
+end
+
+function active_games(api_key::String,
+                      region::String,
+                      encryptedSummonerId::String ;
+                      endpoint::HTTP.URI = lol_api_server(region),
+                      action::Function = http_action)::CurrentGameInfo
+    call_api(CurrentGameInfo, api_key, action, endpoint, "/lol/spectator/v4/active-games/by-summoner/$encryptedSummonerId")
 end
 
 end # module LOLTools.SpectatorV4
