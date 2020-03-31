bce041c8928161864c9a528dac82ee9bb8bc65f1
WooKyoung Noh <wookay.noh@gmail.com>
2020-03-31 15:27:15 +0900

    datadragon 10.6.1

--- gen/common.jl
+++ gen/common.jl
@@ -1,6 +1,6 @@
 # gen/common.jl
 # https://developer.riotgames.com/docs/lol#data-dragon
 
-data_version = "10.5.1"
+data_version = "10.6.1"
 locales = ("en_US", "ko_KR")
 resources = (champion=:Champions, item=:Items, summoner=:SummonerSpells)
