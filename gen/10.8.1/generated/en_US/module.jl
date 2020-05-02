e606f9cdc6bf7b0bf6dbf1ebc58836bd0cceb885
WooKyoung Noh <wookay.noh@gmail.com>
2020-05-02 17:54:42 +0900

    datadragon 10.9.1

--- gen/10.8.1/generated/en_US/module.jl
+++
@@ -1,6 +0,0 @@
-# generated
-module en_US # LOLTools.DataDragon
-include(normpath(@__DIR__, "champion.jl"))
-include(normpath(@__DIR__, "item.jl"))
-include(normpath(@__DIR__, "summoner.jl"))
-end # module LOLTools.DataDragon.en_US
--- gen/10.8.1/generated/ko_KR/module.jl
+++
@@ -1,6 +0,0 @@
-# generated
-module ko_KR # LOLTools.DataDragon
-include(normpath(@__DIR__, "champion.jl"))
-include(normpath(@__DIR__, "item.jl"))
-include(normpath(@__DIR__, "summoner.jl"))
-end # module LOLTools.DataDragon.ko_KR
