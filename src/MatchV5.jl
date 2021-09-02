4814f554ee59d13b70213df77c2107cf3681f01f
WooKyoung Noh <wookay.noh@gmail.com>
2021-09-03 04:52:44 +0900

    fix docs

--- src/MatchV5.jl
+++ src/MatchV5.jl
@@ -4,13 +4,13 @@ using ..LOLTools: AbstractDTO, lol_api_server, http_action, call_api, nothing_in
 using HTTP
 
 """
-Get a list of match ids by puuid
-matches_by_puuid(api_key::String,
-                 region::String,
-                 puuid::String ;
-                 endpoint::HTTP.URI = lol_api_server(region),
-                 action::Function = http_action,
-                 event::Function = nothing_in_event)::Vector{String}
+    Get a list of match ids by puuid
+    matches_by_puuid(api_key::String,
+                     region::String,
+                     puuid::String ;
+                     endpoint::HTTP.URI = lol_api_server(region),
+                     action::Function = http_action,
+                     event::Function = nothing_in_event)::Vector{String}
 """
 function matches_by_puuid(api_key::String,
                           region::String,
@@ -28,13 +28,13 @@ struct MatchDto <: AbstractDTO
 end
 
 """
-Get a match by match id
-match_by_match_id(api_key::String,
-                  region::String,
-                  matchId::String ;
-                  endpoint::HTTP.URI = lol_api_server(region),
-                  action::Function = http_action,
-                  event::Function = nothing_in_event)::MatchDto
+    Get a match by match id
+    match_by_match_id(api_key::String,
+                      region::String,
+                      matchId::String ;
+                      endpoint::HTTP.URI = lol_api_server(region),
+                      action::Function = http_action,
+                      event::Function = nothing_in_event)::MatchDto
 """
 function match_by_match_id(api_key::String,
                            region::String,
@@ -51,13 +51,13 @@ struct MatchTimelineDto <: AbstractDTO
 end
 
 """
-Get a match timeline by match id
-match_timeline_by_match_id(api_key::String,
-                           region::String,
-                           matchId::String ;
-                           endpoint::HTTP.URI = lol_api_server(region),
-                           action::Function = http_action,
-                           event::Function = nothing_in_event)::MatchTimelineDto
+    Get a match timeline by match id
+    match_timeline_by_match_id(api_key::String,
+                               region::String,
+                               matchId::String ;
+                               endpoint::HTTP.URI = lol_api_server(region),
+                               action::Function = http_action,
+                               event::Function = nothing_in_event)::MatchTimelineDto
 """
 function match_timeline_by_match_id(api_key::String,
                                     region::String,
